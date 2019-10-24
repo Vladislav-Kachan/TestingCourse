7149bd94104d72892c7ba6e5215b827dea5c69ab
Vladislav Kachan <Kachan872@mail.ru>
2019-10-24 18:34:14 +0300

    Update Selenium again

--- SeleniumTests/SeleniumTests/Tests.cs
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -1,50 +1,54 @@
 ﻿using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumTests
 {
     [TestFixture]
-    public class Tests : BrouserFunction
+    public class Tests /*: BrouserFunction*/
     {
-        //Ввод неверного номер телефона
-        //Шаги
-        //Зайти на сайт
-        //Перейти налюбое авто
-        //Ввести в поле 'Ваше имя' Vadim
-        //Ввести в поле 'Номер телефона' -3751111111
-        //Ввести в поле 'Email' test@mail.ru
-        //Нажать кнопку 'Забронировать'
-        //Ожидаемый результат: Появление сообщения "Ваше сообщение отправлено!"
+        public IWebDriver webDriver;
+
+        [SetUp]
+        public void OpenBrouserWithSite()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            webDriver.Navigate().GoToUrl("https://car-rent.by/");
+        }
+
+        [TearDown]
+        public void CloseBrouser()
+        {
+            webDriver.Quit();
+        }
+
         [Test]
         public void OrderWithInvalidPhone()
         {
-            var ImageCar = GetWebElementByXPath("//div[@class='elementor-element elementor-element-693b434 elementor-widget elementor-widget-image']");
+            var ImageCar = webDriver.FindElement(By.XPath("//div[@class='elementor-element elementor-element-693b434 elementor-widget elementor-widget-image']"));
             ImageCar.Click();
-            var NameField = GetWebElementByName("field7[]");
-            var PhoneField = GetWebElementByName("field9[]");
-            var MailField = GetWebElementByName("field8");
+            var NameField = webDriver.FindElement(By.Name("field7[]"));
+            var PhoneField = webDriver.FindElement(By.Name("field9[]"));
+            var MailField = webDriver.FindElement(By.Name("field8"));
             NameField.SendKeys("Vadim");
             PhoneField.SendKeys("-3751111111");
             MailField.SendKeys("test@mail.ru");
-            var OrderButton = GetWebElementByXPath("//button[@class='button submit-button']");
+            var OrderButton = webDriver.FindElement(By.XPath("//button[@class='button submit-button']"));
             OrderButton.Click();
-            var CompliteOrder = GetWebElementByXPath("//div[@class='final-success']");
+            var CompliteOrder = webDriver.FindElement(By.XPath("//div[@class='final-success']"));
             Assert.AreEqual(CompliteOrder.Text, "check\r\nВаше сообщение отправлено!");
         }
 
-        //Ввод неверной даты
-        //Шаги
-        //Зайти на сайт
-        //В поле "Марка или модель" ввести Lada
-        //Нажать поиск        
-        //Ожидаемый результат: В списке появяться машины модели Lada
+
         [Test]
         public void OrderWithPastDate()
         {
-            var SearchMarka = GetWebElementByName("phrase");
+            var SearchMarka = webDriver.FindElement(By.Name("phrase"));
             SearchMarka.SendKeys("Lada"+Keys.Enter);
-            var NameCar = GetWebElementByXPath("//h2[@class='entry-title']");            
+            var NameCar = webDriver.FindElement(By.XPath("//h2[@class='entry-title']"));            
             Assert.IsTrue(NameCar.Text.StartsWith("Lada"));            
         }
     }
